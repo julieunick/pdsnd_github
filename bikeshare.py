32e2dc9eb0d063189d4290aa99d1869b3ac181d8
Julie Unick <jlwunick@gmail.com>
2020-12-13 12:19:50 -0600

    Removed unecessary information from load_data() descriptive comment

--- bikeshare.py
+++ bikeshare.py
@@ -53,8 +53,6 @@ def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
 
-    Practice Problem #3 was utilized for this portion.
-
     Args:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
